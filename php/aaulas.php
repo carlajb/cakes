190b31ce162ec35c6ec8efb7f055733fead033fc
carlajb <carla_agencia@hotmail.com>
2020-10-25 18:21:17 -0300

    alteracoes projetos cake

---
+++ php/aaulas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a=4;
+    $b=2;
+    echo $a/$b == 2 ? "o resultado da divisao e dois": "o resultado da divisao nao e dois";
+?>
+    </body>
+
+</html>
